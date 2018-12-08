a71e9f81a6487c95c28ee604d28d70ae93b0e269
JensVanAssche <jens.va@hotmail.com>
2018-12-08 15:05:29 +0100

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <img src="../assets/images/arcon_logo.png" class="nav_logo">
+    <img src="../assets/images/profile_icon.png" class="nav_icon">
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "Navbar"
+  }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <div class="games_container">
+      <img src="../assets/images/game_pacman.png" class="game game_left">
+      <img src="../assets/images/game_pong.png" class="game">
+      <img src="../assets/images/game_tetris.png" class="game game_right">
+
+      <h1>Pong</h1>
+      <p>Pong is gebaseerd op tafeltennis. De naam Pong is afgeleid van pingpong. Het spel werkt met twee batjes en een balletje en twee muren aan de zijkant. De bedoeling van het spel is dat het balletje achter het batje van de tegenspeler terechtkomt.</p>
+    </div>
+</main>
+</template>
+
+<script>
+  export default {
+    name: "Home"
+  }
+</script>
